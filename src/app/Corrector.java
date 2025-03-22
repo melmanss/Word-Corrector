79adc7ce796672ae7cee535002f3746bfc5c9e0a
melmanss <cfhfyjdbx@gmail.com>
2025-03-22 17:33:28 +0200

    new commit

--- src/app/Corrector.java
+++ src/app/Corrector.java
@@ -1,4 +1,18 @@
 package app;
 
 public class Corrector {
+
+    public String handleData(String[] strs) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int count = 1; //з 1 для нумерації
+        for (String str : strs) {
+            // перевірка наявності символу 'b' та його заміна
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+            count++;
+        }
+        return stringBuilder.toString();
+    }
 }
